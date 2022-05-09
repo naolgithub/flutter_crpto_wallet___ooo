78ed4446a1b55576bb2961d2fa11623b338f135b
naolgithub <naolcovered@gmail.com>
2022-05-09 15:00:11 +0300

    home view updated

--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -1,53 +1,80 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_crpto_wallet___ooo/net/api_methods.dart';
 import 'package:flutter_crpto_wallet___ooo/ui/add_view.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
-  State<HomeView> createState() => _HomeViewState();
+  _HomeViewState createState() => _HomeViewState();
 }
 
 class _HomeViewState extends State<HomeView> {
+  double bitcoin = 0.0;
+  double ethereum = 0.0;
+  double tether = 0.0;
+
+  @override
+  initState() {
+    super.initState();
+    updateValues();
+  }
+
+  updateValues() async {
+    bitcoin = await getPrice("bitcoin");
+    ethereum = await getPrice("ethereum");
+    tether = await getPrice("tether");
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
+    getValue(String id, double amount) {
+      if (id == "bitcoin") {
+        return (bitcoin * amount).toStringAsFixed(2);
+      } else if (id == "ethereum") {
+        return (ethereum * amount).toStringAsFixed(2);
+      } else {
+        return (tether * amount).toStringAsFixed(2);
+      }
+    }
+
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
           color: Colors.white,
         ),
-        width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: StreamBuilder(
-          stream: FirebaseFirestore.instance
-              .collection('Users')
-              .doc(FirebaseAuth.instance.currentUser!.uid)
-              .collection('Coins')
-              .snapshots(),
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (!snapshot.hasData) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+        width: MediaQuery.of(context).size.width,
+        child: Center(
+          child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('Users')
+                  .doc(FirebaseAuth.instance.currentUser!.uid)
+                  .collection('Coins')
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
 
-            return ListView(
-              children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                return Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text("Coin Name:${document.id}"),
-                     // Text('Amount Owned: ${document.data()["Amount"]}'),
-                    ],
-                  ),
+                return ListView(
+                  children: snapshot.data!.docs.map((document) {
+                    return Row(
+                      children: [
+                        Text("Coin: ${document.id}"),
+                        Text(
+                            "Price: ${getValue(document.id, document.data()['Amount'])}"),
+                      ],
+                    );
+                  }).toList(),
                 );
-              }).toList(),
-            );
-          },
+              }),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -59,11 +86,11 @@ class _HomeViewState extends State<HomeView> {
             ),
           );
         },
+        backgroundColor: Colors.blue,
         child: const Icon(
           Icons.add,
           color: Colors.white,
         ),
-        backgroundColor: Colors.blue,
       ),
     );
   }
